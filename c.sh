8d505d591590e876cbcd6e346be07262f24c3bfd
s3gm3ntat1onfault <39803948+s3gm3ntat1onfault@users.noreply.github.com>
2018-05-31 19:09:03 -0300

    Delete c.sh

--- c.sh
+++
@@ -1,7 +0,0 @@
-echo "# Double-speed-mod" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/s3gm3ntat1onfault/Double-speed-mod.git
-git push -u origin master
-
